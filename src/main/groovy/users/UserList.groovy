ff6be6b60c2498ab697cfdbb5df4efd4ded7df91
solisoares <solisoareswa@gmail.com>
2023-02-26 14:30:15 -0300

    feat(users): add UserList class

---
+++ src/main/groovy/users/UserList.groovy
@@ -0,0 +1,44 @@
+package users
+
+abstract class UserList {
+    List<User> users = new ArrayList<>()
+
+    List<User> getAll() {
+        return users
+    }
+
+    Integer count() {
+        return users.size()
+    }
+
+    void add(User user) {
+        users.add(user)
+    }
+
+    User get(String name) throws NoSuchElementException {
+        User user
+
+        users.each { ap ->
+            if (ap.getName() == name)
+                user = ap
+        }
+
+        if (!user)
+            throw new NoSuchElementException("The User is not present")
+
+        return user
+    }
+
+    List<User> getBySkill(String skill) {
+        List<User> usersBySkill = []
+        users.each { ap ->
+            if (skill in ap.getSkills())
+                usersBySkill << ap
+        }
+
+        if (!usersBySkill)
+            throw new NoSuchElementException("There are no users with this skill")
+
+        return usersBySkill
+    }
+}
