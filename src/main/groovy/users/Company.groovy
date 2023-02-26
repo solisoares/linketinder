e93939406ece4559d6233009059c6084d02c78f0
solisoares <solisoareswa@gmail.com>
2023-02-26 14:20:54 -0300

    feat(users): add update method in Company

--- src/main/groovy/users/Company.groovy
+++ src/main/groovy/users/Company.groovy
@@ -21,4 +21,30 @@ Company{
     super=${super.toString()}
 }"""
     }
+
+    void update(String newName, String newEmail,
+                String newCountry, String newState,
+                Long newCep, String newDescription,
+                Long newCnpj, List<String> newSkills)
+    {
+        // Get original fields for Elvis Check
+        String originalName = getName()
+        String originalEmail = getEmail()
+        String originalCountry = getCountry()
+        String originalState = getState()
+        Long originalCep = getCep()
+        String originalDescription = getDescription()
+        Long originalCnpj = getCnpj()
+        List<String> originalSkills = getSkills()
+
+        // Set new fields if they are of type Groovy-Truth
+        setName(newName?:originalName)
+        setEmail(newEmail?:originalEmail)
+        setCountry(newCountry?:originalCountry)
+        setState(newState?:originalState)
+        setCep(newCep?:originalCep)
+        setDescription(newDescription?:originalDescription)
+        setCnpj(newCnpj?:originalCnpj)
+        setSkills(newSkills?:originalSkills)
+    }
 }
