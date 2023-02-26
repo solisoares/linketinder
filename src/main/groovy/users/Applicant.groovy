045eadd3d605d2489062a26d0725bebfc620018f
solisoares <solisoareswa@gmail.com>
2023-02-26 14:16:34 -0300

    feat(users): add update method in Applicant

--- src/main/groovy/users/Applicant.groovy
+++ src/main/groovy/users/Applicant.groovy
@@ -26,6 +26,35 @@ Applicant{
     super=${super.toString()}
 }"""
     }
+
+    void update(String newName, String newEmail,
+                String newCountry, String newState,
+                Long newCep, String newDescription,
+                Long newCpf, Integer newAge,
+                List<String> newSkills)
+    {
+        // Get original fields for Elvis Check
+        String originalName = getName()
+        String originalEmail = getEmail()
+        String originalCountry = getCountry()
+        String originalState = getState()
+        Long originalCep = getCep()
+        String originalDescription = getDescription()
+        Long originalCpf = getCpf()
+        Integer originalAge = getAge()
+        List<String> originalSkills = getSkills()
+
+        // Set new fields if they are of type Groovy-Truth
+        setName(newName?:originalName)
+        setEmail(newEmail?:originalEmail)
+        setCountry(newCountry?:originalCountry)
+        setState(newState?:originalState)
+        setCep(newCep?:originalCep)
+        setDescription(newDescription?:originalDescription)
+        setCpf(newCpf?:originalCpf)
+        setAge(newAge?:originalAge)
+        setSkills(newSkills?:originalSkills)
+    }
 }
 
 
